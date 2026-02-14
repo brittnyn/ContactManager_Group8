4a43550c1a4f896f77f7147eb9da9ca95a8feae6
Rine121 <160043616+Rine121@users.noreply.github.com>
2026-02-14 14:58:55 -0500

    Update SearchContacts.php

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -27,7 +27,7 @@
 			}
 			$searchCount++;
 			// This returns a JSON object for each contact
-			$searchResults .= '{"FirstName" : "' . $row["FirstName"] . '", "LastName" : "' . $row["LastName"] . '", "Phone" : "' . $row["Phone"] . '", "Email" : "' . $row["Email"] . '"}';
+			$searchResults .= '{"id" : "' . $row["ID"] . '", "FirstName" : "' . $row["FirstName"] . '", "LastName" : "' . $row["LastName"] . '", "Phone" : "' . $row["Phone"] . '", "Email" : "' . $row["Email"] . '"}';
 		}
 		
 		if( $searchCount == 0 )
