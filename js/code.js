b36df8a8a1fa74a1157e491bda7a4e3f7b0fb8e6
Adam-Disanti <disanti.adam@gmail.com>
2026-02-14 13:35:14 -0500

    Update welcome user message

--- js/code.js
+++ js/code.js
@@ -273,7 +273,7 @@ function readCookie()
 	}
 	else
 	{
-		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+		document.getElementById("userName").innerHTML = firstName + " " + lastName;
 	}
 }
 
